3ceae5bc29e9e27ad70abd4cc3b6a3f8eb29663a
Dilip Vishwakarma <2106dilip@gmail.com>
2025-05-29 03:02:44 +0530

    feat: add deleteProduct functionality and update createProduct to handle multiple images

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -12,20 +12,27 @@ const { uploadOnCloudinary } = require("../utils/Cloudinary")
 
 const createProduct = asyncHandler(async (req, res) => {
     const { productName, description, price, quantity, category } = req.body
-    const imageLocalPath = req?.file?.path
+    const imageLocalPath = req?.files
 
 
     if (!productName || !description || !price || !quantity || !category || !imageLocalPath) {
         throw new ApiError(406, "All fields Required")
     }
-    const image = await uploadOnCloudinary(imageLocalPath, true)
+
+    const image = await Promise.all(imageLocalPath.map(async (img) => {
+        let imglocalpath = img.path
+        return await uploadOnCloudinary(imglocalpath, true)
+    }))
+
+    console.log(image);
+
 
     if (!image) {
         throw new ApiError(406, "Something went wrong while uploading the image")
     }
 
     let productCategory = await Category.findOne({ categoryName: category })
-    
+
 
     if (!productCategory) {
         throw new ApiError(404, "Category Not found")
@@ -47,7 +54,7 @@ const createProduct = asyncHandler(async (req, res) => {
             productId: formattedId,
             productName,
             description,
-            image:image,
+            image,
             price,
             quantity,
             category: productCategory
@@ -64,8 +71,8 @@ const createProduct = asyncHandler(async (req, res) => {
 
 
 const createCategory = asyncHandler(async (req, res) => {
-    const {category} = req.body
-    
+    const { category } = req.body
+
     if (!category) {
         throw new ApiError(406, "category required..!")
     }
@@ -82,8 +89,24 @@ const createCategory = asyncHandler(async (req, res) => {
         )
 })
 
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { productId } = req.body
+    if (!productId) {
+        throw new ApiError(501, "something went wrong")
+    }
+    const deletedProduct = await Product.findOneAndDelete(
+        { productId }
+    )
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200, deletedProduct, "Product Deleted"
+        ))
+})
 
 module.exports = {
     createProduct,
-    createCategory
+    createCategory,
+    deleteProduct
 }
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const { verifyJWT } = require("../middlewares/auth.middleware");
-const { createProduct, createCategory } = require("../controllers/product.controller");
+const { createProduct, createCategory, deleteProduct } = require("../controllers/product.controller");
 const { upload } = require("../middlewares/multer.middleware");
 
 
 const router = Router()
 router.use(verifyJWT)
 
-router.route("/createProduct").post(upload.single("image"), createProduct)
+router.route("/createProduct").post(upload.array("image",4), createProduct)
 router.route("/createCategory").post(createCategory)
+router.route("/deleteProduct").post(deleteProduct)
 
 module.exports = router
