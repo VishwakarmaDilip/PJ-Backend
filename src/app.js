992a9eb1c4bc286fc0452f1f26c3230c9e9b2834
Dilip Vishwakarma <2106dilip@gmail.com>
2025-11-26 21:15:10 +0530

    fix: simplify CORS setup by removing redundant options in app.js

--- src/app.js
+++ src/app.js
@@ -7,21 +7,17 @@ const app = express()
 
 const whiteList = process.env.CORS_ORIGIN_WHITELIST
 
-const corsOptions = {
-  origin: function(origin, callback) {
-    if (!origin || whiteList.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type","Authorization"]
-};
+app.use(cors({
+    origin:function(origin,callback) {
+        if(!origin || whiteList.includes(origin)) {
+            callback(null, true)
+        }else {
+            callback(new Error("Note allowed by CORS"))
+        }
+    },
+    credentials:true
+}))
 
-app.use(cors(corsOptions));
-app.options("*", cors(corsOptions));
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
